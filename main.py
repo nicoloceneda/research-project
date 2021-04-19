25beed9d666ae893f79c1e7f1f409a249a5b2795
nicoloceneda <nicoloceneda@gmail.com>
2021-04-19 21:33:58 +0100

    Update 1

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ random_num = np.random.normal(loc=0, scale=3, size=100000)
 random_int = np.round(random_num)
 
 # Plot:
-axis = np.arange(start=min(random_int), stop = max(random_int) + 1)
-plt.hist(random_int, bins = axis)
+axis = np.arange(start=min(random_int), stop=max(random_int)+1)
+plt.hist(random_int, bins=axis)
+
+m_theta =
