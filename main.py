be3f5ad95241e5ae790dcf58bb6f4e17524f0dc0
nicoloceneda <nicoloceneda@gmail.com>
2021-04-21 01:46:23 +0100

    Update 2

--- main.py
+++ main.py
@@ -7,6 +7,26 @@ random_int = np.round(random_num)
 
 # Plot:
 axis = np.arange(start=min(random_int), stop=max(random_int)+1)
+d_pi = 0
 plt.hist(random_int, bins=axis)
 
-m_theta =
+f =
+delta = 0.9
+p = 0.5
+K = 0.7
+sigma_D = 1
+theta = np.arange(-4000, 7000, 1000)
+
+def kappa
+
+    k =
+def c_theta(x):
+
+
+    c = 1 / ((delta + p + (theta - 1) * x + 0.5 * x * (1 - x) * sigma_D ** 2) * (1 - p * K))
+
+    return c
+
+for gamma in np.arange(1,3,0.5):
+
+    plt.plot(c_theta(gamma))
