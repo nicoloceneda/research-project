79c961e6f7e522e5fd3f75e445c0a28d884d6017
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2021-10-25 00:24:56 +0100

    Add files via upload

--- research.py
+++ research.py
@@ -1,6 +1,6 @@
-""" VERONESI 2000
-    -------------
-    Replication of Veronesi (2000).
+""" STOCHASTIC INTEREST RATE AND LEARNING
+    -------------------------------------
+    Code for the paper Stochastic Interest Rate and Learning.
 """
 
 
@@ -16,14 +16,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# Import custom programs
+
+from stochastic_processes import *
+
+
 # -------------------------------------------------------------------------------
-# UTILITY
+# ECONOMY
 # -------------------------------------------------------------------------------
 
 
-# Utility
+# Utility function u
 
-def utility_fun(consumption_, gamma_):
+def utility_u(consumption_, gamma_):
 
     if gamma_ != 0:
 
@@ -35,25 +40,283 @@ def utility_fun(consumption_, gamma_):
 
     return utility_
 
+""" Comments:
+    * Dividing by gamma_ guarantees that utility increases in consumption also for risk averse investors.
+    * Adding the 1 guarantees that utility starts at zero for every gamma_.
+"""
 
-# Plot utility function
+# Plot utility function u
 
-consumption = np.arange(0, 5, 0.05)
-gammas = [0.0, 0.5, 1.0, 1.5]
+consumption = np.arange(0, 1, 0.01)
+gamma = [-1.5, 0.0, 1.5]
 
-utility = pd.DataFrame(columns=gammas)
+utility = pd.DataFrame(columns=gamma)
 
-for g in gammas:
+for g in gamma:
 
-    utility.loc[:, g] = utility_fun(consumption, g)
+    utility.loc[:, g] = utility_u(consumption, g)
 
 fig_1, ax = plt.subplots(nrows=1, ncols=1, figsize=(5.5, 4))
-ax.plot(consumption, utility, label=gammas)
+ax.plot(consumption, utility, label=gamma)
 ax.set_ylabel('U(C)', fontsize=10)
 ax.set_xlabel('C', fontsize=10)
 ax.set_title('CARA Utility',fontsize=10)
 ax.legend(loc='upper left', fontsize=10)
 ax.set_xlim(xmin=np.min(consumption), xmax=np.max(consumption))
-ax.set_ylim(ymin=0, ymax=np.max(utility.iloc[:,0]))
+ax.set_ylim(ymin=0, ymax=np.max(utility.values))
 fig_1.tight_layout()
 fig_1.savefig('images/fig_1.png')
+
+
+# Utility function h
+
+def utility_h(consumption_, psi_):
+
+    if psi_ != 0:
+
+        utility_ = (1 - np.exp(- 1 / psi_ * consumption_)) * psi_
+
+    else:
+
+        utility_ = consumption_
+
+    return utility_
+
+
+# Plot utility function h
+
+consumption = np.arange(0, 1, 0.01)
+psi = [-1.5, 0.0, 1.5]
+
+utility = pd.DataFrame(columns=psi)
+
+for p in psi:
+
+    utility.loc[:, p] = utility_h(consumption, p)
+
+fig_2, ax = plt.subplots(nrows=1, ncols=1, figsize=(5.5, 4))
+ax.plot(consumption, utility, label=psi)
+ax.set_ylabel('U(C)', fontsize=10)
+ax.set_xlabel('C', fontsize=10)
+ax.set_title('Exponential Utility',fontsize=10)
+ax.legend(loc='upper left', fontsize=10)
+ax.set_xlim(xmin=np.min(consumption), xmax=np.max(consumption))
+ax.set_ylim(ymin=0, ymax=np.max(utility.values))
+fig_2.tight_layout()
+fig_2.savefig('images/fig_2.png')
+
+
+# -------------------------------------------------------------------------------
+# PHASE DIAGRAM
+# -------------------------------------------------------------------------------
+
+
+# Gamma = 1 / psi
+
+def Qparams(theta2_, theta1_, p12_, p21_, pi_, sigmaD_, gamma_, rate_, delta_=1):
+
+    h_ = (theta2_ - theta1_) / sigmaD_ * pi_ * (1 - pi_)
+    pi2_ = p12_ / (p12_ + p21_)
+    Gamma_pi_ = (theta2_ - theta1_) / (rate_ * (rate_ + p12_ + p21_))
+    
+    Q3_ = h_ ** 2 / 2
+    Q2_ = h_ * sigmaD_ * gamma_ - (p12_ + p21_) * (pi2_ - pi_) + gamma_ * rate_ * Gamma_pi_ * h_ ** 2
+    Q1_ = (rate_ * gamma_) ** 2 / 2 * Gamma_pi_ ** 2 * h_ ** 2 + rate_ * gamma_ ** 2 * sigmaD_ * Gamma_pi_ * h_ + rate_ * np.log(delta_)
+
+    return Q3_, Q2_, Q1_
+
+def parabola(y2_, Q3_, Q2_, Q1_, rate_):
+
+    y1_locus_ = - y2_ ** 2 * Q3_ / rate_ - y2_ * Q2_ / rate_ - Q1_ / rate_
+
+    return y1_locus_
+
+# Parameters
+
+pi = 0.50
+theta2 = 0.05
+theta1 = -0.05
+p12 = 0.20
+p21 = 0.02 #0.012
+sigmaD = 0.10
+rate = 0.10
+delta = 1
+gamma = 4.5
+
+# Phase diagram
+
+y2 = np.arange(-5.0, 5.1, 0.1)
+y1_lim_zero = y2 * p12 / rate - np.log(delta)
+y1_lim_one = - y2 * p21 / rate - np.log(delta)
+
+
+pi_list = [0.5, 0.05, 0.3, 0.7, 0.95]
+y1_locus = pd.DataFrame(columns=pi_list)
+
+for pi in pi_list:
+
+    Q3, Q2, Q1 = Qparams(theta2, theta1, p12, p21, pi, sigmaD, gamma, rate)
+    y1_locus.loc[:, pi] = parabola(y2, Q3, Q2, Q1, rate)
+
+
+# Plot phase diagram
+
+fig_1, ax = plt.subplots(nrows=1, ncols=1, figsize=(5.5, 4))
+ax.plot(y2, y1_locus.loc[:, 0.5], color='blue')
+ax.plot(y2, y1_locus.iloc[:, 1:], color='blue', linestyle='dashed', linewidth=0.5)
+ax.plot(y2, y1_lim_zero, color='red')
+ax.plot(y2, y1_lim_one, color='red')
+ax.hlines(y=0, xmin=np.min(y2), xmax=np.max(y2), color='black', linestyles='dashed')
+ax.vlines(x=0, ymin=np.min(np.min(y1_locus)), ymax=np.max(np.max(y1_lim_zero)), color='black', linestyles='dashed')
+ax.arrow(x=2.5, y=2.5*p12/rate, dx=0, dy=-0.5*p12/rate, head_width = 0.1)
+ax.arrow(x=2.5, y=2.0*p12/rate, dx=0, dy=-7.5, head_width = 0.1)
+ax.arrow(x=2.5, y=-1.5*p12/rate, dx=0, dy=-3.6, head_width = 0.1)
+ax.arrow(x=2.8, y=-7.5, dx=0, dy=0.7, head_width = 0.1)
+ax.arrow(x=2.8, y=-6.7, dx=0, dy=5.2, head_width = 0.1)
+ax.arrow(x=2.8, y=-1.3, dx=0, dy=0.5, head_width = 0.1)
+plt.text(3.1, -13.5, '$\psi(0.5,y_2)$', fontsize=8)
+plt.text(2.5, 7.8, r'$y_1=\frac{p_{12}}{r} y_2$', fontsize=8)
+plt.text(-4.5, 1.5, r'$y_1=-\frac{p_{21}}{r} y_2$', fontsize=8)
+plt.text(3.9, 5.2, '$\psi(0.05,y_2)$', fontsize=6)
+plt.text(3.9, -3.0, '$\psi(0.95,y_2)$', fontsize=6)
+plt.text(3.9, -6, '$\psi(0.30,y_2)$', fontsize=6)
+plt.text(3.9, -9.5, '$\psi(0.70,y_2)$', fontsize=6)
+ax.set_ylabel('$x_1=f$', fontsize=10)
+ax.set_xlabel('$x_2=f^\prime$', fontsize=10)
+ax.set_title(r'Phase Diagram $\gamma=\frac{1}{\psi}$',fontsize=10)
+ax.set_xlim(xmin=np.min(y2), xmax=np.max(y2))
+ax.set_ylim(ymin=np.min(np.min(y1_locus)), ymax=np.max(np.max(y1_lim_zero)))
+fig_1.savefig('images/phase_diagram.png')
+
+
+
+
+
+
+# -------------------------------------------------------------------------------
+# PROPOSITION 1
+# -------------------------------------------------------------------------------
+
+
+# Price
+
+thetas = np.arange(0.00, 0.31, 0.001)
+sigmaDs = np.arange(0.00, 0.31, 0.001)
+interest = np.arange(0.01, 0.101, 0.001)
+gammas = [0.0, 0.5, 1.0, 1.5]
+
+def Pt_1(theta, sigmaD, interest, gamma, dividend):
+
+    Pt = - gamma * sigmaD ** 2 / interest ** 2 + dividend / interest + theta / interest ** 2
+
+    return Pt
+
+Pt_interest = pd.DataFrame(columns=gammas)
+
+for j in gammas:
+
+        Pt_interest.loc[:, j] = Pt_1(0.05, 0.25, interest, j, 1)
+
+plt.plot(interest, Pt_interest)
+
+
+# Value function
+
+
+def Jt_1(delta, psi, rate, gamma, sigmaD, wealth):
+
+    Jt = - delta / psi * np.exp(1 - delta / (rate) - np.log(rate) - gamma * sigmaD ** 2 / (2 * psi * rate) - rate / psi * wealth)
+
+    return Jt + 1
+
+delta = 0.05
+sigmaD = 0.2
+
+psi = 1.5
+gamma = 1.5
+rates = np.arange(0.01, 0.101, 0.001)
+gammas = [0.0, 0.5, 1.0, 1.5]
+psis = [0.5, 1.0, 1.5, 2.0]
+
+
+Jt_rate_gamma = pd.DataFrame(columns=gammas)
+
+for g in gammas:
+
+    Jt_rate_gamma.loc[:, g] = Jt_1(delta=delta, psi=psi, rate=rates, gamma=g, sigmaD=sigmaD, wealth=1)
+
+Jt_rate_psi = pd.DataFrame(columns=psis)
+
+for p in psis:
+
+    Jt_rate_psi.loc[:, p] = Jt_1(delta=delta, psi=p, rate=rates, gamma=gamma, sigmaD=sigmaD, wealth=1)
+
+fig_2, ax = plt.subplots(nrows=1, ncols=2, figsize=(11, 4))
+
+ax[0].plot(rates, Jt_rate_gamma, label=gammas)
+ax[0].set_ylabel('Value function ($J_t$)', fontsize=10)
+ax[0].set_xlabel('Interest rate ($r$)', fontsize=10)
+ax[0].set_title('Value Function For different levels of ARA',fontsize=10)
+ax[0].legend(loc='upper right', fontsize=10)
+ax[0].set_xlim(xmin=np.min(rates), xmax=np.max(rates))
+
+ax[1].plot(rates, Jt_rate_psi, label=psis)
+ax[1].set_ylabel('Value function ($J_t$)', fontsize=10)
+ax[1].set_xlabel('Interest rate ($r$)', fontsize=10)
+ax[1].set_title('Value Function as a Function of EIS',fontsize=10)
+ax[1].legend(loc='upper right', fontsize=10)
+ax[1].set_xlim(xmin=np.min(rates), xmax=np.max(rates))
+
+fig_2.tight_layout()
+fig_2.savefig('images/fig_2.png')
+
+
+delta = 0.05
+sigmaD = 0.2
+
+psi = 1.5
+gamma = 1.5
+rates = np.arange(0.01, 0.101, 0.001)
+gammas = [0.0, 0.5, 1.0, 1.5]
+psis = [0.5, 1.0, 1.5, 2.0]
+
+
+Jt_rate_gamma = pd.DataFrame(columns=gammas)
+
+for g in gammas:
+
+    Jt_rate_gamma.loc[:, g] = Jt_1(delta=delta, psi=psi, rate=rates, gamma=g, sigmaD=sigmaD, wealth=1)
+
+Jt_rate_psi = pd.DataFrame(columns=psis)
+
+for p in psis:
+
+    Jt_rate_psi.loc[:, p] = Jt_1(delta=delta, psi=p, rate=rates, gamma=gamma, sigmaD=sigmaD, wealth=1)
+
+fig_2, ax = plt.subplots(nrows=1, ncols=2, figsize=(11, 4))
+
+ax[0].plot(rates, Jt_rate_gamma, label=gammas)
+ax[0].set_ylabel('Value function ($J_t$)', fontsize=10)
+ax[0].set_xlabel('Interest rate ($r$)', fontsize=10)
+ax[0].set_title('Value Function For different levels of ARA',fontsize=10)
+ax[0].legend(loc='upper right', fontsize=10)
+ax[0].set_xlim(xmin=np.min(rates), xmax=np.max(rates))
+
+ax[1].plot(rates, Jt_rate_psi, label=psis)
+ax[1].set_ylabel('Value function ($J_t$)', fontsize=10)
+ax[1].set_xlabel('Interest rate ($r$)', fontsize=10)
+ax[1].set_title('Value Function as a Function of EIS',fontsize=10)
+ax[1].legend(loc='upper right', fontsize=10)
+ax[1].set_xlim(xmin=np.min(rates), xmax=np.max(rates))
+
+fig_2.tight_layout()
+fig_2.savefig('images/fig_2.png')
+
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import numpy as np
+
+
